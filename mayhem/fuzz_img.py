de278db9494c77c76c3f859d304eb2f4aa7e13c2
Bailey Capuano <bcapuano@asu.edu>
2022-11-01 23:44:15 -0400

    Bug fix

--- mayhem/fuzz_img.py
+++ mayhem/fuzz_img.py
@@ -1,29 +1,40 @@
-#! /usr/bin/python3
+#!/usr/bin/env python3
 import atheris
+import contextlib
 import sys
 import io
-from PIL import Image, UnidentifiedImageError
+import traceback
+from PIL import Image
 
-with atheris.instrument_imports():
+with atheris.instrument_imports(exclude=['sympy*']):
     from primify.base import PrimeImage
 
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    yield
+    sys.stdout = save_stdout
 
 
 @atheris.instrument_func
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
-    byte_count = 4096
-    image_length = 2 ** fdp.ConsumeIntInRange(1, 3)
-    image_width = byte_count // image_length
+    
+    f = io.BytesIO(data)
+    f.seek(0)
 
     try:
-        image = Image.frombytes("L", (image_length, image_width), fdp.ConsumeBytes(byte_count))
-        resized_img = PrimeImage.resize_for_pixel_limit(image, fdp.ConsumeIntInRange(128, 4095))
-        quant_img = PrimeImage.quantize_image(resized_img)
-        PrimeImage.quantized_image_to_number(quant_img)
-    except (UnidentifiedImageError, ValueError) as e:
-        if isinstance(e, ValueError) and "magic number" not in str(e) and "tile cannot" not in str(e) and "not enough" not in str(e):
-            raise
+        image = Image.open(f)
+        with nostdout():
+            quant_img = PrimeImage.quantize_image(image)
+            PrimeImage.quantized_image_to_number(quant_img)
+    except Exception as e:
+        if 'PIL' in traceback.format_exc():
+            return -1
+        raise
+
 
 
 def main():
